bdb060080af24d576fe8d71e6145f2de93cc402c
Uttam Bhattarai <tb2040@rit.edu>
2025-10-04 18:56:37 -0400

    Temperature 4 - reject temperature below absolute zero in Kelvin : creating failing test

--- src/test/java/edu/rit/swen352/tdd/easy/TemperatureTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/TemperatureTest.java
@@ -54,4 +54,12 @@ class TemperatureTest {
         });
     }
 
+    @Test
+    @DisplayName("4: Reject temperature below absolute zero in Kelvin")
+    void testRejectBelowAbsoluteZeroKelvin() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Temperature(-1.0, Temperature.TemperatureUnit.KELVIN);
+        });
+    }
+
 }
