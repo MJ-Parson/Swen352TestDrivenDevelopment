8cf8173a063e7064f39966e4d003b0843aa126f8
yp5751 <113635140+yp5751@users.noreply.github.com>
2025-10-05 23:44:18 -0400

    MyStack : 2 : construct stack with default capacity : create failing test

--- src/test/java/edu/rit/swen352/tdd/hard/MyStackTest.java
+++ src/test/java/edu/rit/swen352/tdd/hard/MyStackTest.java
@@ -9,12 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
  * Test suite for the {@link MyStack} component.
  */
 class MyStackTest {
-    
+
     @Test
     @DisplayName("Constructor Test, Create Stack with Specified Capacity")
     public void testConstructor() {
         int capacity = 10;
         MyStack<String> stack = new MyStack<>(capacity);
+        assertInstanceOf(MyStack.class, stack);
+    }
+
+    @Test
+    @DisplayName("Constructor Test, Create Stack with Default Capacity of 16")
+    public void testConstructorDefault() {
+        MyStack<Integer> stack = new MyStack<>();
         assertInstanceOf(MyStack.class, stack);
     }
 
